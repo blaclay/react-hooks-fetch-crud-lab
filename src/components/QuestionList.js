5f200a033ef42719cca7145db29a841137561f94
Clay Blackwelder <clayblackwelder@gmail.com>
2023-10-18 18:16:08 -0700

    testing

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,6 +1,15 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [QuestionItem] = useState("All");
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((questions) => setQuestions(questions));
+  }, []);
+
   return (
     <section>
       <h1>Quiz Questions</h1>
